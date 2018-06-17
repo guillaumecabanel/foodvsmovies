d955d7d0afc9cd6e95a73e924ef972bc6337f2c2
Guillaume Cabanel <guillaume.cabanel@gmail.com>
2018-06-17 18:30:07 +0200

    add bulma and komponent

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,8 @@ Bundler.require(*Rails.groups)
 
 module Foodvsmovies
   class Application < Rails::Application
+    config.i18n.load_path += Dir[config.root.join('frontend/components/**/*.yml')]
+    config.autoload_paths << config.root.join('frontend/components')
     config.generators do |generate|
           generate.assets false
           generate.helper false
